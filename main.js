b5b9e141ea15617de02c852446b748a02532def6
AmeenAbuAlnasser <96382716+AmeenAbuAlnasser@users.noreply.github.com>
2023-12-26 14:57:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//document.querySelector(".st2").classList.remove("st2");
+var st1= document.querySelector(".st1");
+st1.classList.add("demo");
+ var vip=document.querySelector(".vip");
+ vip.classList.remove("vip");
